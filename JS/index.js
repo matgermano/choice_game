9c894d3cbc678e4156a2888f9f39cdb595a2cb37
matgermano <88158544+matgermano@users.noreply.github.com>
2021-08-11 15:22:18 -0300

    Organização do projeto em diferentes pastas, realocação dos arquivos

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+var abc = prompt("texto de pergunta qui")
+
+If (variavel = 1){
+Return location = "esquerda.html"
+}
+If else (variável = 2) {
+Return location = "direita.html"
+}
+else {
+Return alert = digite uma opção válida
+}
