38406ae36e0d0444a6fdb7596676282ea89d7dff
matgermano <88158544+matgermano@users.noreply.github.com>
2021-08-13 00:07:42 -0300

    Página de escolha de personagens estilizada com Js pra ser iniciado, próxima etapa é preparar a pág inicial do jogo.

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,54 @@
-var abc = prompt("texto de pergunta qui")
+var personagem = parseInt(prompt("Escolha seu personagem \n" + '1-Arturo \n' + '2-Thoman \n' + '3-Tessete'));
 
-If (variavel = 1){
-Return location = "esquerda.html"
+if (personagem == 1){
+    document.write('Siga esse link:');
+
+}else if (personagem == 2){
+     document.write('Siga esse link:');
+
+}else if (personagem == 3) {
+     document.write('Siga esse link:');
 }
-If else (variável = 2) {
-Return location = "direita.html"
-}
-else {
-Return alert = digite uma opção válida
-}
+
+else{
+    alert('Deixa de ser lerdão e escolhe uma opção entre as 3 oferecidas!')    
+} 
+
+
+
+    // function pulaLinha() {
+
+    //     document.write("<br>");
+    // }
+
+    // function mostra(frase) {
+
+    //     document.write(frase);
+    //     pulaLinha();
+    // }
+
+    // function sorteia(n) {
+
+    //     return Math.round(Math.random() * n);
+    // }    
+
+    // var numeroPensado = sorteia(10);
+
+
+    //  var chute = parseInt(prompt("Já pensei. Qual você acha que é?"));
+
+    // if(chute == numeroPensado) {
+
+    //     mostra("Uau! Você acertou, pois eu pensei no " + numeroPensado);
+    // } else {
+
+    //     if(chute > numeroPensado) {
+
+    //         mostra("Você errou! Seu chute é maior que o número pensado!");
+
+    //     } else {
+
+    //         mostra("Você errou! Seu chute é menor que o número pensado!");
+    //     }
+
+    // }
